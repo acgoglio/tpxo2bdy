c702fd0c03b83f6fafc382adfb6916ccdfde83de
Anna Chiara Goglio <ag15419@n046.cluster.net>
2021-04-02 16:12:01 +0200

    Last run_bdy_txt.sh version

--- run_bdy_txt.sh
+++ run_bdy_txt.sh
@@ -10,6 +10,11 @@ set -e
 #set -x 
 ########################
 echo "*********** Tidal bdy 4 EAS System *********"
+
+####################### EXTRACTION FROM TPXO ############################################
+
+echo "Extraction from TPXO model.."
+
 # Load the environment
 module load intel19.5/19.5.281 impi19.5/19.5.281 impi19.5/netcdf/C_4.7.2-F_4.5.2_CXX_4.3.1
 
@@ -27,7 +32,7 @@ OUTBDY_TEMPLATE_TAB_TEMP='bdytide_tab_grid%grid%.txt'
 OUTBDY_TEMPLATE_PRE='mfs_bdytide'
 echo "Input bdy file templat: ${INBDY_TEMPLATE}"
 echo "Intermediate txt table: ${OUTBDY_TEMPLATE_TAB}"
-:
+
 # Outfields to grids connection
 OUTFIELD=("z" "u" "v")
 INGRIDS=("T" "U" "V") 
@@ -50,7 +55,6 @@ for ID_FIELDGRID in 0 1 2; do
    # Build the outfiles
    echo "I am building the outfile templates.."
    for ID_TCOMP in ${TIDAL_U_COMPONENTS[@]}; do
-       echo "prova $ID_TCOMP"
        OUTBDY=${OUTBDY_TEMPLATE_PRE}_${ID_TCOMP}_grid_${INGRIDS[$ID_FIELDGRID]}.nc
        cp $INBDY ${OUTBDY}
        echo "${OUTBDY} ready"
@@ -79,5 +83,31 @@ for ID_FIELDGRID in 0 1 2; do
 
    # Read the field into the tab and write it into the nc file
    
-   # Clean the directory
 done 
+
+# Clean the directory
+rm tmp_*.txt
+
+####################### TIDAL BDY FIELDS BUILDING  ############################################
+
+echo "Tidal bdy files building.."
+
+# Load the environment
+#module purge
+#module load anaconda
+#source activate mappyenv
+
+B_EXE_NAME='tbdy_writer.py'
+python ${B_EXE_NAME}
+
+# Set title and author in the outfiles and remove extra fields
+module purge
+module load anaconda/3.7 curl/7.70.0 cmake/3.17.3 gams/28.2.0 gcc_9.1.0/9.1.0 gcc_9.1.0/gempack/12.885 gcc_9.1.0/OpenBLAS/0.3.9 gcc_9.1.0/papi/6.0.0 gcc_9.1.0/R/3.6.1 modules mysql/5.7.28 ncl/6.6.2 sqlite/3.32.2 subversion/1.14.0 wgrib/1.8.1.0b impi20.1/19.7.217 impi20.1/esmf/8.0.1-intelmpi-64-g impi20.1/hdf5/1.12.0 impi20.1/hdf5-threadsafe/1.12.0 impi20.1/netcdf/C_4.7.4-F_4.5.3_CXX_4.3.1 impi20.1/netcdf-threadsafe/C_4.7.4-F_4.5.3_CXX_4.3.1 impi20.1/papi/6.0.0 impi20.1/parallel-netcdf/1.12.1 impi20.1/petsc/3.13.2 impi20.1/zoltan/3.8 intel20.1/20.1.217 intel20.1/advisor intel20.1/boost/1.73.0 intel20.1/cdo/1.9.8 intel20.1/cnvgrib/3.1.1 intel20.1/eccodes/2.17.0 intel20.1/esmf/8.0.1-mpiuni-64-g intel20.1/esmf/8.0.1-mpiuni-64-O intel20.1/exactextract/545f0d6 intel20.1/g2lib/3.1.0 intel20.1/gdal/3.1.0 intel20.1/hdf5/1.12.0 intel20.1/hdf5-threadsafe/1.12.0 intel20.1/inspector intel20.1/itac intel20.1/libemos/4.5.9 intel20.1/libemos/4.5.9 intel20.1/magics/3.3.1 intel20.1/nco/4.9.3 intel20.1/ncview/2.1.8 intel20.1/netcdf/C_4.7.4-F_4.5.3_CXX_4.3.1 intel20.1/netcdf-threadsafe/C_4.7.4-F_4.5.3_CXX_4.3.1 intel20.1/proj/7.0.1 intel20.1/R/4.0.2 intel20.1/szip/2.1.1 intel20.1/udunits/2.2.26 intel20.1/valgrind/3.16.0 intel20.1/vtune intel20.1/w3lib/2.0.6 intel20.1/wgrib2/2.0.8
+
+for TOBEMOD in $( ls ${OUTBDY_TEMPLATE_PRE}*.nc ); do
+    mv $TOBEMOD ${TOBEMOD}_tmp
+    ncatted -O -h -a title,global,m,c,"tidal bdy data from TPXO model" ${TOBEMOD}_tmp
+    ncatted -O -h -a author,global,m,c,"Anna Chiara Goglio - CMCC" ${TOBEMOD}_tmp
+    cdo delete,name=sossheig,vobtcrtx,vobtcrty ${TOBEMOD}_tmp $TOBEMOD
+    rm ${TOBEMOD}_tmp
+done
